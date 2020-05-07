915c0ab3e8f3318b301df12d3ddca338fd0caf7d
ddebode <ddebode@bol.com>
2020-05-07 17:55:41 +0200

    Added for loop to show options

--- scripts/read.sh
+++ scripts/read.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 arr=("htop" "vi")
+for ((i = 0; i < ${#arr[@]}; ++i)); do
+    position=$i 
+    echo "[$position]: ${arr[$i]}"
+done
+
 read -n 1 -p "Make a choice: " input
 echo "input: " ${arr[$input]}
 ${arr[$input]}
--- scripts/tmux_toggle.sh
+++ scripts/tmux_toggle.sh
@@ -5,5 +5,5 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # echo $CURRENT_DIR
 # run-shell /home/ddebode/development/tmux-toggle/scripts/choose.sh
 
-tmux split-window -c "#{pane_current_path}" "$CURRENT_DIR/choose.sh"
+tmux split-window -c "#{pane_current_path}" "$CURRENT_DIR/read.sh"
 tmux resize-pane -Z
