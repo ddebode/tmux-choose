088d457e747c191245dd68bdb27171bb3ed49277
ddebode <ddebode@bol.com>
2020-05-07 18:04:38 +0200

    made choices from 1 and not zero

--- scripts/read.sh
+++ scripts/read.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-arr=("htop" "vi")
+arr=("htop" "vi" "lazygit")
 for ((i = 0; i < ${#arr[@]}; ++i)); do
-    position=$i 
+    position=$(( $i + 1 ))
     echo "[$position]: ${arr[$i]}"
 done
 
 read -n 1 -p "Make a choice: " input
-echo "input: " ${arr[$input]}
-${arr[$input]}
+echo "input: " ${arr[$(($input-1))]}
+${arr[$(($input-1))]}
