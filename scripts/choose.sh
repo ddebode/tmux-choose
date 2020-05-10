d7ebc192e39bd04a07d58c856f26af97f15bf9e9
ddebode <ddebode@gmail.com>
2020-05-10 11:54:22 +0200

    added banner

--- scripts/choose.sh
+++ scripts/choose.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Get options 
 options=$(tmux show-option -gqv @choose-options)
 if [[ -z $options ]]
 then
@@ -8,11 +9,24 @@ else
     IFS=',' read -r -a arr <<< $options
 fi  
 
+
+# Output options
+echo " _______  __   __  __   __  __   __         _______  __   __  _______  _______  _______  _______ "
+echo "|       ||  |_|  ||  | |  ||  |_|  |       |       ||  | |  ||       ||       ||       ||       |"
+echo "|_     _||       ||  | |  ||       | ____  |       ||  |_|  ||   _   ||   _   ||  _____||    ___|"
+echo "  |   |  |       ||  |_|  ||       ||____| |       ||       ||  | |  ||  | |  || |_____ |   |___ "
+echo "  |   |  |       ||       | |     |        |      _||       ||  |_|  ||  |_|  ||_____  ||    ___|"
+echo "  |   |  | ||_|| ||       ||   _   |       |     |_ |   _   ||       ||       | _____| ||   |___ "
+echo "  |___|  |_|   |_||_______||__| |__|       |_______||__| |__||_______||_______||_______||_______|"
+echo ""
+
 for ((i = 0; i < ${#arr[@]}; ++i)); do
     position=$(( $i + 1 ))
     echo "[$position]: ${arr[$i]}"
 done
 
-read -n 1 -p "Make a choice: " input
 
+# Get input and execute choice
+# TODO more -n options should depend on lenght of options
+read -n 1 -p "Make a choice: " input
 ${arr[$(($input-1))]}
